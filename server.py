2cd9c36d2c4deb9fecb31e625d6ca5730762f124
Mitch Henline <rmhenline@gmail.com>
2023-07-04 10:08:38 -0600

    second html added and linked

--- server.py
+++ server.py
@@ -7,6 +7,9 @@ app = Flask(__name__)
 def homepage():
     return render_template('index.html')
 
+@app.route('/pizzaeaten')
+def pizza_eaten():
+    return render_template('pizza_eaten.html')
 
 
 if __name__ == "__main__":
